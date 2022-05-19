9ed6056935855a5fddbc7bfa876993cddc87608b
arallain <75613930+Pataubeur@users.noreply.github.com>
2022-05-19 09:44:10 +0200

    Modification du constructeur Tile

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -4,5 +4,9 @@ import java.awt.image.BufferedImage;
 
 public class Tile {
 	public BufferedImage image;
-	public boolean collision = false;
+	public boolean collision;
+
+	Tile(boolean collision) {
+		this.collision = collision;
+	}
 }

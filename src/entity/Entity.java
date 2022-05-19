a3455b81513f0ee0391bd340f86b25013a9d7c27
FenrirWolf4566 <bdezordo@gmail.com>
2022-05-19 09:51:07 +0200

    Modification Entity Création Monsters et SimpleMonster

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -6,4 +6,9 @@ public class Entity {
 	public int x,y;
 	public int speed;
 	public BufferedImage idleImage;
+
+	// Entities attributs
+	private Integer lifePoint;
+	private Integer attackPoint;
+	private Integer velocityPoint;
 }
